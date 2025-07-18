4f8aca9ec371bb572a1a6905a9b11ab22fc77ee7
Buse <btuna4024@gmail.com>
2025-07-18 16:48:52 +0300

    commit

--- pixels/app/urls.py
+++ pixels/app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import include, path
-
+from django.contrib.auth import views as auth_views
+from app import views 
 from app import views
 
 
 urlpatterns = [
     path('', views.index, name='index'),  
     path('map/', views.map, name='map'),
-    path('api/save-cells/', views.save_selected_cells, name='save_selected_cells')
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='app/logout.html'), name='logout'),
 ]
--- pixels/app/views.py
+++ pixels/app/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from pyexpat.errors import messages
+from django.shortcuts import redirect, render
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.forms import UserCreationForm
+# app/views.py
+from .forms import UsernameOnlyUserCreationForm  # ✅ DOĞRU
+
 from .models import SelectedCell
 
 
@@ -27,3 +32,17 @@ def save_selected_cells(request):
             SelectedCell.objects.create(col=col, row=row)
         return JsonResponse({'status': 'success', 'saved': len(cells)})
     return JsonResponse({'status': 'error', 'message': 'Only POST allowed'}, status=400)
+
+def register(request):
+    if request.method == 'POST':
+        form = UsernameOnlyUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+     #       messages.success(request, "Kayıt başarılı!")
+            return redirect('login')  # veya 'index'
+        else:
+            print("Form hataları:", form.errors)  # Hataları terminale yaz
+          #  messages.error(request, "Form geçersiz.")
+    else:
+        form = UsernameOnlyUserCreationForm()
+    return render(request, 'app/register.html', {'form': form})
