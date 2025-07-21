a27abc6440fae11debfdae319fe7152e75014136
Buse <btuna4024@gmail.com>
2025-07-21 10:25:12 +0300

    düzeltme

--- pixels/app/models.py
+++ pixels/app/models.py
@@ -6,3 +6,15 @@ class SelectedCell(models.Model):
 
     def __str__(self):
         return f"Cell ({self.col}, {self.row})"
+
+
+# class SelectedCellForUser(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     col = models.IntegerField()
+#     row = models.IntegerField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+#     class Meta:
+#         unique_together = ('user', 'col', 'row')
+
+# Veritabanına kaydetmedim.
